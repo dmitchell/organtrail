bf862ecb74534e6932790653aff2f244cbe8c35e
Don Mitchell <dmitchell@edx.org>
2013-01-26 13:28:48 -0500

    patient selection

--- src/organtrail/controller/organtrail.py
+++ src/organtrail/controller/organtrail.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+import random
+
+available_players = range(1, 10)
+npc_players = []
 
 def home(request):
-    return render_to_response('organtrail.html', {})
+    return render_to_response('organtrail.html', 
+                              { "player" : random.choice(available_players)})
